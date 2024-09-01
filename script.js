29c6293c4f6a2b013402a6e1be3484a3a4981eb4
Cleberson Rocha <clebersonrochaads@gmail.com>
2024-08-31 22:40:06 -0300

    Style: Add responsivity and adjust media screen on project

--- script.js
+++ script.js
@@ -10,3 +10,30 @@ allSideMenu.forEach(item => {
         li.classList.add('active');
     })
 })
+
+const menuBar = document.querySelector('#content nav .bx.bx-menu');
+const sideBar = document.getElementById('sidebar');
+
+menuBar.addEventListener('click', function() {
+    sideBar.classList.toggle('hide');
+})
+
+if (window.innerWidth < 768) {
+    sidebar.classList.add('hide');
+}
+
+const searchButton = document.querySelector('#content nav form .form-input button');
+const searchButtonIcon = document.querySelector('#content nav form .form-input button .bx');
+const searchForm = document.querySelector('#content nav form');
+
+searchButton.addEventListener('click', function(e) {
+    if (window.innerWidth < 576) {
+        e.preventDefault();
+        searchForm.classList.toggle('show');
+        if (searchForm.classList.contains('show')) {
+            searchButtonIcon.classList.replace('bx-search', 'bx-x');
+        } else {
+            searchButtonIcon.classList.replace('bx-x', 'bx-search');
+        }
+    }
+})
