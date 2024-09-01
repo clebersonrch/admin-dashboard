002389c8d10c644fbd8285a9421213af7d283f55
Cleberson Rocha <clebersonrochaads@gmail.com>
2024-08-31 23:09:16 -0300

    Style: Add graphics data and set the style of project

--- script.js
+++ script.js
@@ -18,10 +18,6 @@ menuBar.addEventListener('click', function() {
     sideBar.classList.toggle('hide');
 })
 
-if (window.innerWidth < 768) {
-    sidebar.classList.add('hide');
-}
-
 const searchButton = document.querySelector('#content nav form .form-input button');
 const searchButtonIcon = document.querySelector('#content nav form .form-input button .bx');
 const searchForm = document.querySelector('#content nav form');
@@ -35,5 +31,19 @@ searchButton.addEventListener('click', function(e) {
         } else {
             searchButtonIcon.classList.replace('bx-x', 'bx-search');
         }
+    }
+})
+
+if (window.innerWidth < 768) {
+    sidebar.classList.add('hide');
+} else if (window.innerWidth > 576) {
+    searchButtonIcon.classList.replace('bx-x', 'bx-search');
+    searchForm.classList.remove('show');
+}
+
+window.addEventListener('resize', function () {
+    if (this.innerWidth > 576) {
+        searchButtonIcon.classList.replace('bx-x', 'bx-search');
+        searchForm.classList.remove('show');
     }
 })
